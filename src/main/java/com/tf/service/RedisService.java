4f51cdc25c98d2d2b819d73bcd196d2418a596e6
Lin <2487049041@qq.com>
2020-01-22 19:33:37 +0800

    feat: 增加Redis工具类

---
+++ src/main/java/com/tf/service/RedisService.java
@@ -0,0 +1,78 @@
+package com.tf.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lgq
+ * @date 2020/1/22
+ */
+@Service
+public interface RedisService {
+
+    /**
+     * 设置
+     * @param key 键
+     * @param value 值
+     */
+    void set(String key, Object value);
+
+    /**
+     * 设置
+     * @param key 键
+     * @param value 值
+     * @param time 过期时间
+     */
+    void setex(String key, Object value, long time);
+
+    /**
+     * 自增
+     * @param key 键
+     * @return 自增后的值
+     */
+    Long incr(String key);
+
+    /**
+     * 自减
+     * @param key 键
+     * @return 自检后的值
+     */
+    Long decr(String key);
+
+    /**
+     * 获取数据
+     * @param key 键
+     * @param clazz 返回对象烈性
+     * @param <T>  泛型
+     * @return value
+     */
+    <T> T get(String key, Class<T> clazz);
+
+    /**
+     * 设置值并返回旧值
+     * @param key 键
+     * @param newValue 新值
+     * @param clazz 类对象
+     * @param <T> 泛型
+     * @return 旧值
+     */
+    <T> T getset(String key, Object newValue, Class<T> clazz);
+
+    /**
+     * hash设置
+     * @param key 键
+     * @param field 键
+     * @param value 值
+     */
+    void hset(Object key, Object field, Object value);
+
+    /**
+     * hash获取
+     * @param key 键
+     * @param field 键
+     * @param clazz 类
+     * @param <T> 泛型
+     * @return 值
+     */
+    <T> T hget(Object key, Object field, Class<T> clazz);
+
+}
---
+++ src/main/java/com/tf/service/impl/RedisServiceImpl.java
@@ -0,0 +1,89 @@
+package com.tf.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.tf.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+/**
+ * @author lgq
+ * @date 2020/1/22
+ */
+public class RedisServiceImpl implements RedisService {
+
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private final RedisTemplate redisTemplate;
+
+    private static int AUTO_INCREMENT = 1;
+
+    @Autowired
+    public RedisServiceImpl(StringRedisTemplate stringRedisTemplate, RedisTemplate redisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void set(String key, Object value) {
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        stringValueOperations.set(key, JSON.toJSONString(value));
+    }
+
+    @Override
+    public void setex(String key, Object value, long time) {
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        stringValueOperations.set(key, JSON.toJSONString(value), time);
+    }
+
+    @Override
+    public Long incr(String key) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.increment(key);
+    }
+
+    @Override
+    public Long decr(String key) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.decrement(key);
+    }
+
+    @Override
+    public <T> T get(String key, Class<T> clazz) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        String string = valueOperations.get(key);
+        if (string != null) {
+            return JSON.parseObject(string, clazz);
+        }
+        return null;
+    }
+
+    @Override
+    public <T> T getset(String key, Object newValue, Class<T> clazz) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        String oldValue = valueOperations.getAndSet(key, JSON.toJSONString(newValue));
+        if (oldValue != null) {
+            return JSON.parseObject(oldValue, clazz);
+        }
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void hset(Object key, Object field, Object value) {
+        if (key instanceof String && field instanceof String
+            && value instanceof String) {
+            redisTemplate.opsForHash().put(key, field, value);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T hget(Object key, Object field, Class<T> clazz) {
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        return (T)hashOperations.get(key, field);
+    }
+}
