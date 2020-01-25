5b3ef2c84d61d42c4d68d0c28477320f346e26be
Lin <2487049041@qq.com>
2020-01-25 21:51:14 +0800

    fix:抛出Exception不能正常捕获

--- src/main/java/com/tf/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/tf/advice/GlobalExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.tf.advice;
 
+import com.tf.constant.CodeMessage;
 import com.tf.exception.GlobalException;
 import com.tf.vo.BaseResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,13 +15,20 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
-    @ExceptionHandler(value = GlobalException.class)
-    public BaseResult<String> handlerAdException(HttpServletRequest request,
-                                                 GlobalException ex) {
-        BaseResult<String> response = new BaseResult<>();
-        response.setCode(ex.getCodeMessage().getCode());
-        response.setMsg(ex.getMessage());
-        return response;
+    @ExceptionHandler(value = Exception.class)
+    public BaseResult<String> handlerException(HttpServletRequest request,
+                                                 Exception ex) {
+        if (ex instanceof GlobalException) {
+            GlobalException globalException = (GlobalException) ex;
+            BaseResult<String> response = new BaseResult<>();
+            response.setCode(globalException.getCodeMessage().getCode());
+            response.setMsg(globalException.getMessage());
+            return response;
+        } else {
+            return new BaseResult<>(CodeMessage.INTERNAL_SERVER_ERROR.getCode(),
+                    CodeMessage.INTERNAL_SERVER_ERROR.getMessage());
+        }
+
     }
 
 }
